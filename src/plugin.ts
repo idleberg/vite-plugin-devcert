666c195756fef9095235003acda7128f6ff7b5fd
Jan T. Sott <git@idleberg.com>
2025-05-31 23:30:35 +0200

    chore: remove linebreaks

--- src/plugin.ts
+++ src/plugin.ts
@@ -36,9 +36,9 @@ export default function DevcertPlugin(options: PluginOptions = {}): Plugin {
 			console.info(
 				// Keep lines short for better readability
 				[
-					`\n${logSymbols.info} Generating certificate for "${domain}".`,
+					`${logSymbols.info} Generating certificate for "${domain}".`,
 					'You may be prompted to enter your password to allow the creation of root certificate authority.',
-					'\nPlease refer to documentation at https://github.com/expo/devcert#how-it-works for details.\n',
+					'\nPlease refer to documentation at https://github.com/expo/devcert#how-it-works for details.',
 				].join(' '),
 			);
 
