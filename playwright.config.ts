742c657746d07b926eed3116c448bf1516bd69f6
Jan T. Sott <git@idleberg.com>
2025-06-27 13:12:59 +0200

    test: add retry setting

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { env } from 'node:process';
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+	retries: 3,
 	testDir: 'e2e',
 	use: {
 		trace: 'on-first-retry',
